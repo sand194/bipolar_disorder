0f008a659e8a9ec4cc91e0fd44eb3bbadccc07b1
kz302268 <kz302268@student.polsl.pl>
2024-06-23 00:05:14 +0200

    TODO

---
+++ main.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+df = pd.read_csv("C:/Users/Lenovo/source/repos/bipolar_disorder/data/dataset.csv")
+
+print(df)
+
+#TODO:
+#Najpierw chcemy zrobic analize tej tabelki
+#accuracy 100%
+#zapisujemy model
+#mamy stworzyc funkcje ktora tworzy profil uzytkownika
+#ta funkcja zadaje wszystkie pytania z datasetu, i ocenia na ile jest chory psychicznie
